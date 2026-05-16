32da75b73cd7450ee920760549a1f5d9a716e008
Ramiro Tonelli <tonelliramiro748@gmail.com>
2026-05-16 02:15:05 -0300

    Polling para el front

--- BackEnd/ProductorAPI/Productora/Program.cs
+++ BackEnd/ProductorAPI/Productora/Program.cs
@@ -120,7 +120,8 @@ builder.Services.AddCors(option =>
     option.AddPolicy("AllowFront", policy =>
     {
         policy.WithOrigins(
-            "http://localhost:5252",
+            "http://localhost:5252", 
+            "http://127.0.0.1:5500",
             "https://productora-api.vercel.app",
             "https://ripply-pregastrular-barrett.ngrok-free.dev"
             )
